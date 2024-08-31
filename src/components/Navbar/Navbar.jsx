b9962cb542e948b0d0840842a645a2fc36757e39
omnandurkar <nandurkarom172@gmail.com>
2024-08-31 15:59:34 +0530

    ref: resume added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,21 +4,21 @@ const Navbar = () => {
   return (
     <>
 
-    
 
-     
-      <header class="flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full py-7">
+
+
+      {/* <header class="flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full py-7">
         <nav id='navbar' class="relative max-w-7xl w-full flex flex-wrap md:grid md:grid-cols-12 basis-full items-center px-4 md:px-6  mx-auto" >
           <div class="md:col-span-3">
-          
+
             <a class="flex-none rounded-xl text-xl inline-block font-semibold focus:outline-none "  >
               <h1>Om <span className='opacity-60'>Nandurkar</span> </h1>
             </a>
-        
-          </div>
 
- 
-          {/* <div class="flex items-center gap-x-2 ms-auto py-1 md:ps-6 md:order-3 md:col-span-3">
+          </div> */}
+
+
+      {/* <div class="flex items-center gap-x-2 ms-auto py-1 md:ps-6 md:order-3 md:col-span-3">
             <button type="button" class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-xl border border-gray-200 text-white hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none">
               Sign in
             </button>
@@ -33,10 +33,10 @@ const Navbar = () => {
               </button>
             </div>
           </div> */}
-   
 
 
-          {/* <div id="navbar-collapse-with-animation" class="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow md:block md:w-auto md:basis-auto md:order-2 md:col-span-6">
+
+      {/* <div id="navbar-collapse-with-animation" class="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow md:block md:w-auto md:basis-auto md:order-2 md:col-span-6">
             <div class="flex flex-col gap-y-4 gap-x-0 mt-5 md:flex-row md:justify-center md:items-center md:gap-y-0 md:gap-x-7 md:mt-0">
               <div>
                 <a class="relative inline-block text-white before:absolute before:bottom-0.5 before:start-0 before:-z-[1] before:w-full before:h-1 before:bg-lime-400" href="#" aria-current="page">Work</a>
@@ -56,6 +56,24 @@ const Navbar = () => {
             </div>
           </div> */}
 
+      {/* <h2 className='flex  text-end'> Resume</h2> */}
+
+      {/* </nav>
+      </header> */}
+
+      <header>
+        <nav className='p-10 flex  justify-between'>
+          <a className="flex-none rounded-xl text-xl inline-block font-semibold focus:outline-none "  >
+            <h1>Om <span className='opacity-60'>M Nandurkar</span> </h1>
+          </a>
+
+
+
+          <a href='https://drive.google.com/file/d/13c049p0rHmRTWH_BSrOAUta5o70aKYQS/view?usp=sharing' target='_black'>
+            <h2 className='p-2 bg-slate-100 opacity-75 hover:opacity-100 text-xs uppercase  text-black font-bold  font-rale active:scale-90 cursor-pointer  rounded-md border'>
+              Resume
+            </h2>
+          </a>
         </nav>
       </header>
 
