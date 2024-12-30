34c020cba500a6f7c07a4859fe3db34c4012db21
omnandurkar <nandurkarom172@gmail.com>
2024-12-30 23:17:04 +0530

    ref: resume updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -73,11 +73,16 @@ const Navbar = () => {
               )
             }
 
-            <a href='https://drive.google.com/file/d/1bh8vXwP7kQGtycHlwlQIneSkYD9XyNI5/view?usp=sharing' target='_black'>
+            <a href='https://drive.google.com/file/d/1-ee3ilUBnDORaGhk-VJi6N-68vaRCt90/view?usp=sharing' target='_black'>
               <h2 className='p-2 bg-slate-100 flex justify-center items-center  opacity-75 hover:opacity-100 text-xs uppercase  text-black font-bold  font-rale active:scale-90 cursor-pointer  rounded-md border'>
                 Resume
               </h2>
             </a>
+            {/* <a href='https://drive.google.com/file/d/1bh8vXwP7kQGtycHlwlQIneSkYD9XyNI5/view?usp=sharing' target='_black'>
+              <h2 className='p-2 bg-slate-100 flex justify-center items-center  opacity-75 hover:opacity-100 text-xs uppercase  text-black font-bold  font-rale active:scale-90 cursor-pointer  rounded-md border'>
+                Resume
+              </h2>
+            </a> */}
 
           </div>
 
